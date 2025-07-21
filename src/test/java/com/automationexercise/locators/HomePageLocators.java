3a13c2da788137094f57d6a819d56d181d08f320
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-07-21 17:40:37 +0530

    HomePage Locators added

---
+++ src/test/java/com/automationexercise/locators/HomePageLocators.java
@@ -0,0 +1,7 @@
+package com.automationexercise.locators;
+
+import org.openqa.selenium.By;
+
+public class HomePageLocators extends NavbarLocators {
+	public static final By SLIDER_CAROUSEL = By.id("slider-carousel");
+}
