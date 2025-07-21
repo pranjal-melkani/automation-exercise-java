f7ae694fdec8040ace0d1216b22e0e505086b802
Pranjal Melkani <pranjal.melkani18@gmail.com>
2025-07-21 17:34:32 +0530

    Locators for Navbar links updated

---
+++ src/test/java/com/automationexercise/locators/NavbarLocators.java
@@ -0,0 +1,14 @@
+package com.automationexercise.locators;
+
+import org.openqa.selenium.By;
+
+public class NavbarLocators {
+	public static final By HOME = By.partialLinkText("Home");
+	public static final By PRODUCTS = By.partialLinkText("Products");
+	public static final By CART = By.partialLinkText("Cart");
+	public static final By SIGNUP_LOGIN = By.partialLinkText("Signup / Login");
+	public static final By TESTCASES = By.partialLinkText("Test Cases");
+	public static final By API_TESTING = By.partialLinkText("API Testing");
+	public static final By VIDEO_TUTORIALS = By.partialLinkText("Video Tutorials");
+	public static final By CONTACT_US = By.partialLinkText("Contact us");
+}
