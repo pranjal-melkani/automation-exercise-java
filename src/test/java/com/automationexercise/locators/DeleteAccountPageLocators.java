e1b181eb78f43aa2d2254c91cb8dfbea7f06a10d
Pranjal <pranjal.melkani18@gmail.com>
2025-08-01 11:05:15 +0530

    Delete Account page methods/locators added

---
+++ src/test/java/com/automationexercise/locators/DeleteAccountPageLocators.java
@@ -0,0 +1,7 @@
+package com.automationexercise.locators;
+
+import org.openqa.selenium.By;
+
+public class DeleteAccountPageLocators extends NavbarLocators {
+	public static final By ACCOUNT_DELETED_MSG = By.xpath("//h2[@data-qa='account-deleted']");
+}
